a7bee8530aa1aaa5d3e8bf674cdb58b25defe941
addie <addiebogdon@gmail.com>
2026-08-24 20:05:48 -0500

    added user input scanner

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,54 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         ArrayList<Integer> arr = new ArrayList<Integer>();
-        arr.add(1);
-        arr.add(3);
+
+        while (true) {
+            System.out.println("Enter a series of numbers separated by a comma: ");
+
+            arr.clear();
+
+            String input = scan.nextLine();
+            String[] inputArr = input.split("\\s*,\\s*");
+
+            for (String str : inputArr) {
+                try {
+                    arr.add(Integer.parseInt(str));
+                } catch (NumberFormatException e) {
+                    System.out.println("Skipping invalid number format " + str);
+                }
+            }
+
+            System.out.println("You entered " + arr + ", is that correct? Y/N: ");
+            String response = scan.nextLine();
+
+            if (response.equalsIgnoreCase("y")) {
+                break;
+            }
+        }
 
         int even = 0;
         int odd = 0;
 
-        for(int i : arr){
-            if(i % 2 == 0){
+        for (int i : arr) {
+            if (i % 2 == 0) {
                 even++;
-            }else{
+            } else {
                 odd++;
             }
         }
-        if(even > odd){
+        if (even > odd) {
             System.out.println("EVEN");
-        }
-        else if(odd > even){
+        } else if (odd > even) {
             System.out.println("ODD");
-        }else{
+        } else {
             System.out.println("NEUTRAL");
         }
+        scan.close();
     }
 }
